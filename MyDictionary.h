ec6999263986d0f93dafabdd117d0f14c1aa6b42
kotovro <kotvoro@gmail.com>
2025-04-13 01:12:31 +0300

    Refactors MyDictionary and adds red black tree class

--- MyDictionary.h
+++ MyDictionary.h
@@ -1,1 +1,28 @@
 #pragma once
+
+template <typename TKey, typename TValue>
+class MyDicitionary
+{
+public:
+
+	MyDictionary();
+
+
+	bool insert(std::pair<TKey, TValue>)
+	{
+
+	};
+
+	bool deleteValue(TKey kyValue)
+	{
+
+	};
+
+	TValue search(TKey keyValue)
+	{
+
+	};
+
+private:
+
+};
